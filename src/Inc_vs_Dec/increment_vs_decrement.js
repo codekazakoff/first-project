3614f1ce9682f348f61830f726cfd70eee79edd2
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-09 10:17:03 +0500

    first add commit

--- src/Inc_vs_Dec/increment_vs_decrement.js
+++ src/Inc_vs_Dec/increment_vs_decrement.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
 class Increment_VS_Decrement extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter : 0
+    }
+  }
+
+  handleIncrement = ()=> {
+    const {counter} = this.state;
+    this.setState({
+      counter : counter + 1
+    })
+  }
+  handleDecrement = ()=> {
+    const {counter} = this.state;
+    this.setState({
+      counter : counter - 1
+    })
+  }
+  
   render() {
+    console.log("salom increment",this.state.counter);
+    const { counter } = this.state;
+    const {handleIncrement,handleDecrement} = this;
     return (
-      <>
-        <h1>salom bu oddiy h1</h1>
-      </>
+      <React.Fragment>
+        <h2>salom bu oddiy h1</h2>
+        <span style={{marginRight : "1rem"}}>{counter}</span>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </React.Fragment>
     );
   }
 }
