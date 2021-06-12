aa22d1cece25b5dbadbf2de62b288f722a4f8985
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-12 12:02:39 +0500

    like complated

--- src/masalalar/homework/homework.js
+++ src/masalalar/homework/homework.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "../../css/homework/homework.css";
 import { getMovies } from "../../services/fakeServis";
-
+import {AiFillHeart,AiOutlineHeart} from 'react-icons/ai';
+import like from "../../assists/img/like.png";
+import dislike from "../../assists/img/dislike.png";
 class HomeWork extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cinemas: getMovies(),
-    };  
+    };
   }
   handleDelete = (id) => {
     const { cinemas } = this.state;
@@ -15,10 +17,16 @@ class HomeWork extends Component {
       cinemas: cinemas.filter((cinema) => cinema.id !== id),
     });
   };
+  handleLike = (id) => {
+    const { cinemas } = this.state;
+    const movies = cinemas.find( movie => movie.id === id);
+    movies.isLiked = movies.isLiked ? false : true;
+    this.setState({ cinemas});
+  };
   render() {
     const { cinemas } = this.state;
     const count = cinemas.length;
-    const { handleDelete } = this;
+    const { handleDelete, handleLike } = this;
     return (
       <>
         <section>
@@ -38,13 +46,16 @@ class HomeWork extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {cinemas.map(({ id, title, genre, stock, rate }) => (
+                  {cinemas.map(({ id, title, genre, stock, rate, isLiked }) => (
                     <tr key={id}>
                       <td>{title}</td>
                       <td>{genre}</td>
                       <td>{stock}</td>
                       <td>{rate}</td>
-                      <td>
+                      <td style={{cursor : 'pointer'}}>
+                        <span onClick={() => handleLike(id)}>
+                        {isLiked ? <AiOutlineHeart  size="2rem" color="red"/> : <AiFillHeart size="2rem" color="red"/>}
+                        </span>
                         <button onClick={() => handleDelete(id)}>Delete</button>
                       </td>
                     </tr>
